01803036c9c44dc0ae21d133e0f3fadd012fe5ef
Liam Kelly <lkelly3@crimson.ua.edu>
2024-09-19 15:37:54 -0500

    fixed links

--- Fall2024-Assignment1-lkelly3/Fall2024-Assignment1-lkelly3/wwwroot/js/site.js
+++ Fall2024-Assignment1-lkelly3/Fall2024-Assignment1-lkelly3/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// JavaScript source code
+function Diego() {
+    // What to do
+    window.open('https://en.wikipedia.org/wiki/Diego_Luna', '_blank');
+}
+
+function Felicity() {
+    // What to do
+    window.open('https://en.wikipedia.org/wiki/Felicity_Jones', '_blank');
+}
+
+function Ben() {
+    // What to do
+    window.open('https://en.wikipedia.org/wiki/Ben_Mendelsohn', '_blank');
+}
+
+function Mads() {
+    // What to do
+    window.open('https://en.wikipedia.org/wiki/Mads_Mikkelsen', '_blank');
+}
+
+function Alan() {
+    // What to do
+    window.open('https://en.wikipedia.org/wiki/Alan_Tudyk', '_blank');
+}
